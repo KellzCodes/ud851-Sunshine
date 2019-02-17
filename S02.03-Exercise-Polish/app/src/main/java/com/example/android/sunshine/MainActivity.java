a0063430d9b4afec4aadf508f3df3edc908e6eb6
Kelli Davis <keldavis5@yahoo.com>
2019-02-17 00:51:32 -0500

    feat: Create a reference to the error display textview

--- S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mWeatherTextView;
 
     // TODO (6) Add a TextView variable for the error message display
+    TextView errorMessageTextView;
 
     // TODO (16) Add a ProgressBar variable to show and hide the progress bar
 
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (7) Find the TextView for the error message using findViewById
+        errorMessageTextView = findViewById(R.id.error_message_display);
 
         // TODO (17) Find the ProgressBar using findViewById
 
