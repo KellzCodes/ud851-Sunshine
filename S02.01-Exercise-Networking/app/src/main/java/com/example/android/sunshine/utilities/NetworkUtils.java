93eeea180d8bca5a63d19da94628f820b5edaec5
Kelli Davis <keldavis5@yahoo.com>
2019-02-16 23:50:41 -0500

    fix: Make URI builder return url instead of null

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -16,6 +16,7 @@
 package com.example.android.sunshine.utilities;
 
 import android.net.Uri;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -82,7 +83,9 @@ public final class NetworkUtils {
         }catch (MalformedURLException e){
             e.printStackTrace();
         }
-        return null;
+
+        Log.v(TAG, "Built URI " + url);
+        return url;
     }
 
     /**
